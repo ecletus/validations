a074b26807f9a0197ff3fc06e867c5151f98da83
Moises P. Sena <moisespsena@gmail.com>
2019-03-21 14:58:57 -0300

    Rename organization

--- validation_test.go
+++ validation_test.go
@@ -9,8 +9,8 @@ import (
 	"github.com/asaskevich/govalidator"
 	"github.com/moisespsena-go/aorm"
 	_ "github.com/mattn/go-sqlite3"
-	"github.com/aghape/core/test/utils"
-	"github.com/aghape/validations"
+	"github.com/ecletus/core/test/utils"
+	"github.com/ecletus/validations"
 )
 
 var db *aorm.DB
